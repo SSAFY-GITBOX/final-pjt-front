734e4634048dd031aa97f9310c504eb64b46e54d
seungtaeryu <dbtmdxo1992@naver.com>
2022-11-15 17:52:11 +0900

    SignUp 페이지 구현

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,44 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import router from '@/router'
 
 Vue.use(Vuex)
 
+const API_URL = 'http://127.0.0.1:8000'
+
 export default new Vuex.Store({
   state: {
+    token: null,
   },
   getters: {
   },
   mutations: {
+    // 회원가입 && 로그인
+    SAVE_TOKEN(state, token) {
+      state.token = token
+      router.push({ name: 'ArticleView' })
+    }
   },
   actions: {
+    signUp(context, payload) {
+      axios({
+        method: 'post',
+        url: `${API_URL}/accounts/signup/`,
+        data: {
+          username: payload.username,
+          password1: payload.password1,
+          password2: payload.password2,
+        }
+      })
+        .then((res) => {
+          console.log(res)
+          context.commit('SAVE_TOKEN', res.data.key)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
   },
   modules: {
   }
