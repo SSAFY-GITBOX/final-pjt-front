6d20923fc8dfb46fe8487c38e7c738317956f3ac
soheemoon37 <tnghgml37@gmail.com>
2022-11-19 21:31:35 +0900

    navbar 적용

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,50 @@
 <template>
   <div id="app">
-    <nav>
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <img src="./assets/gitbox_logo.png" alt="" height="40">
+        <a class="navbar-brand" href="#" style="margin-left: 3px; font-size: large;">GITBOX</a>
+        <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button> -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent" style="display: flex; justify-content: space-between;">
+          <div>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
+                <router-link class="nav-link active" aria-current="page" :to="{ name: 'HomeView' }">Home</router-link>
+              </li>
+              <li class="nav-item">
+                <!-- <a class="nav-link" href="#">Link</a> -->
+                <router-link class="nav-link" :to="{ name: 'ArticleView' }">Community</router-link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+          <div>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <router-link class="nav-link" :to="{ name: 'SignUpView' }">SignUp</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" :to="{ name: 'LogInView' }">LogIn</router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <!-- <nav>
       <router-link :to="{ name: 'HomeView' }">Home</router-link> |
 			<router-link :to="{ name: 'ArticleView' }">Community</router-link> |
       <router-link :to="{ name: 'SignUpView' }">SIGN UP</router-link> |
 			<router-link :to="{ name: 'LogInView' }">LogIn</router-link>
-    </nav>
+    </nav> -->
     <router-view/>
     <!-- <router-view :key="$route.fullPath"/> -->
   </div>
@@ -18,9 +57,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background-color: black;
-  color: white;
-  height: 100vh;
 }
 
 nav {
@@ -33,6 +69,8 @@ nav a {
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  font-weight: bold;
+  color: #2c3e50;
 }
+
 </style>
