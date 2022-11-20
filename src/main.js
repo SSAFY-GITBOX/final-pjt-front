35593bcaaeca5f6fa1aa5478d816c3220c58a3b6
becoding96 <becoding96@gmail.com>
2022-11-21 00:32:22 +0900

    git-grass & like-movie list & like-article list

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueCalendarHeatmap from 'vue-calendar-heatmap'
 
 Vue.use(BootstrapVue)
+Vue.use(VueCalendarHeatmap)
 
 Vue.config.productionTip = false
 
