af1421a9f54f8e7a6e34c7e69d0adbb71d4175f5
becoding96 <becoding96@gmail.com>
2022-11-24 04:59:18 +0900

    recommend movie test

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import MovieDetailView from '../views/MovieDetailView.vue'
 import ArticleDetailView from '../views/ArticleDetailView.vue'
 import ProfileView from '../views/ProfileView.vue'
 import EditProfileView from '../views/EditProfileView.vue'
+import TmpView from '../views/TmpView.vue'
 
 // const originalPush = VueRouter.prototype.push;
 // VueRouter.prototype.push = function push(location) {
@@ -80,6 +81,12 @@ const routes = [
     name: 'EditProfileView',
     component: EditProfileView
   },
+
+  {
+    path: '/tmp/:id',
+    name: 'TmpView',
+    component: TmpView
+  }
 ]
 
 const router = new VueRouter({
