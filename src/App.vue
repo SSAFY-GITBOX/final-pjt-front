33e6e02cdfd802f5bb686841493b9b057f183052
becoding96 <becoding96@gmail.com>
2022-11-24 21:13:49 +0900

    logout btn color update

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@
         <b-navbar-nav v-if="$store.getters.isLogin" style="display: flex;">
           <b-nav-item href="#"><router-link class="nav-link" :to="{ name: 'ProfileView', params: { id: this.$store.state.userPk } }">Profile</router-link></b-nav-item>
           <!-- 로그아웃이 버튼이라 토글창에서 계속 중앙으로 안 갔는데 margin 넣으니까 중앙정렬됨 -->
-          <b-nav-item href="#" style="margin: auto;"><button class="nav-link" @click="logOut" style="background-color: #2e3133; border: none; transform: none;">LogOut</button></b-nav-item>
+          <b-nav-item href="#" style="margin: auto;"><button class="nav-link" @click="logOut" style="background-color: rgb(36, 41, 47); border: none; transform: none;">LogOut</button></b-nav-item>
         </b-navbar-nav>
         <b-navbar-nav v-else style="display: flex;">
           <b-nav-item href="#"><router-link class="nav-link" :to="{ name: 'SignUpView' }">SignUp</router-link></b-nav-item>
